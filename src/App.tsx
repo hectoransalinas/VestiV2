41246442301f72896ea1a73ad88e0d917956e618
Hector Salinas <s04hs@intra.clvsa.com.ar>
2026-01-29 20:49:52 -0300

    fix(sizeguide): add React import to prevent 'React is not defined' blank modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductPageVestiDemo from "./components/ProductPageVestiDemo";
 
 type ProductFromShopify = {
